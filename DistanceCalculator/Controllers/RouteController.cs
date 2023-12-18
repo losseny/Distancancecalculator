2716bfad1775e06b14427cd6d717b954c8476334
losseny <lossenyd46@gmail.com>
2023-12-18 18:17:03 +0100

    gitignore

--- DistanceCalculator/Controllers/RouteController.cs
+++ DistanceCalculator/Controllers/RouteController.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using DistanceCalculator.Model;
 using Flurl.Http;
 using Microsoft.AspNetCore.Cors;
@@ -12,18 +13,12 @@ public class BingMapsController : ControllerBase
 {
 
     [HttpGet]
-    public async Task<BingMapsApiResponse> Get([FromQuery(Name = "departure")] string departure, [FromQuery(Name = "destination")] string destination)
+    [SuppressMessage("ReSharper.DPA", "DPA0011: High execution time of MVC action", MessageId = "time: 928ms")]
+    public Task<BingMapsApiResponse> Get([FromQuery(Name = "departure")] string departure, [FromQuery(Name = "destination")] string destination)
     {
         const string bingMapsKey = "Ahk8EB2_cVR61M2LUmpPOaN3AcRhaghzXYGAlqVWivCQfOfNR8x-0ls51WEo-PVF";
-        const string startPoint = "Schiedam Centrum, 3112 NA Schiedam";
-        const string endPoint = "Muiderpoort, Sarphatistraat, 1018 AV Amsterdam";
         var url = $"http://dev.virtualearth.net/REST/V1/Routes?wayPoint.1={departure}&wayPoint.2={destination}&key={bingMapsKey}";
-
-        Console.WriteLine(departure);
-        Console.WriteLine(destination);
-        Console.WriteLine(url);
-
-        return await url.GetJsonAsync<BingMapsApiResponse>();
+        return url.GetJsonAsync<BingMapsApiResponse>();
     }
 
 }
